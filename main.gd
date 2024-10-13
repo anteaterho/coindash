5775369e7bc1fc5f6282ba376ec067a28b1a97b5
Seungho Choi <anteaterho@gmail.com>
2024-10-13 13:10:35 +0900

    catus logic edit

--- main.gd
+++ main.gd
@@ -3,6 +3,7 @@ extends Node
 @export var coin_scene : PackedScene
 @export var playtime = 30
 @export var powerup_scene : PackedScene
+@export var cactus_scene : PackedScene
 
 var level = 1
 var score = 0
@@ -27,6 +28,13 @@ func spawn_coins():
 		c.screensize = screensize
 		c.position = Vector2(randi_range(0,  screensize.x), randi_range(0, screensize.y))
 
+func spawn_cactus():
+	for i in level -1:
+		var c = cactus_scene.instantiate()
+		add_child(c)
+		c.screensize = screensize
+		c.position = Vector2(randi_range(0,  screensize.x), randi_range(0, screensize.y))
+
 func _on_game_timer_timeout():
 	time_left -= 1
 	$HUD.update_timer(time_left)
@@ -40,13 +48,14 @@ func game_over():
 	playing = false
 	$GameTimer.stop()
 	get_tree().call_group("coins", "queue_free")
+	get_tree().call_group("obstacles", "queue_free")
 	$HUD.show_game_over()
 	$Player.die()
 	
 func _on_hud_start_game():
 	new_game()
 
-# Called when the node enters the scene tree for the first time.
+# Called when the node en짐ters the scene tree for the first time.
 func _ready() -> void:
 	screensize = get_viewport().get_visible_rect().size
 	$Player.screensize = screensize
@@ -58,9 +67,12 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if playing and get_tree().get_nodes_in_group("coins").size() == 0:
-		level + 1
+		level += 1
+		print(level)
 		time_left += 5
 		spawn_coins()
+		get_tree().call_group("obstacles", "queue_free") ## 코인을 다 먹으면 선인장도 모두 지워
+		spawn_cactus()
 		$PowerupTimer.wait_time = randf_range(5,18)
 		$PowerupTimer.start()
 
